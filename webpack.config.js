2e74cc311288a4bd4b4e1dd04891cf1bdb4a3898
Matias Delgado <matiasdel@gmail.com>
2017-03-07 21:07:17 -0300

    Upgrade packages, add style lint

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const pkg = require('./package.json');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const stylelint = require('stylelint');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 const glob = require('glob');
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -24,28 +24,22 @@ const common = {
 		app: PATHS.app
 	},
 	resolve: {
-		extensions: ['', '.js', '.jsx']
+		extensions: ['.js', '.jsx']
 	},
 	output: {
 		path: PATHS.build,
 		filename: '[name].js'
 	},
 	module: {
-		preLoaders: [
-			{ test: /\.css$/, loaders: ['postcss'], include: PATHS.app }
-		],
-		loaders: [
-			{ test: /\.jsx?$/, loaders: ['babel?cacheDirectory'], include: PATHS.app }
+		rules: [
+			{ test: /\.jsx?$/, use: ['babel-loader?cacheDirectory'], include: PATHS.app }
 		]
 	},
-	postcss: function() {
-		return [stylelint({
-			rules: {
-				'color-hex-case': 'lower'
-			}
-		})]
-	},
 	plugins: [
+        new StyleLintPlugin({
+            configFile: './stylelint.json',
+            files: 'app/**/*.css'
+        }),
 		new HtmlWebpackPlugin({
 			template: 'node_modules/html-webpack-template/index.ejs',
 			title: 'Kanban app',
@@ -65,17 +59,14 @@ if (TARGET === 'start' || !TARGET) {
 			historyApiFallback: true,
 			hot: true,
 			inline: true,
-			progress: true,
 			stats: 'errors-only',
 			host: process.env.HOST,
 			port: process.env.PORT
 		},
 		module: {
-            preLoaders: [
-                { test: /\.jsx?$/, loaders: ['eslint'], include: PATHS.app }
-            ],
-			loaders: [
-				{ test: /\.css$/, loaders: ['style', 'css'], include: PATHS.app }
+			rules: [
+                { test: /\.jsx?$/, use: ['eslint-loader'], include: PATHS.app, enforce: 'pre' },
+				{ test: /\.css$/, use: ['style-loader', 'css-loader'], include: PATHS.app }
 			]
 		},
 		plugins: [
@@ -100,11 +91,9 @@ if (TARGET === 'build' || TARGET === 'stats') {
             chunkFilename: '[chunkhash].js'
         },
 		module: {
-            preLoaders: [
-                { test: /\.jsx?$/, loaders: ['eslint'], include: PATHS.app }
-            ],
-			loaders: [
-				{ test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css'), include: PATHS.app }
+			rules: [
+                { test: /\.jsx?$/, use: ['eslint'], include: PATHS.app, enforce: 'pre' },
+				{ test: /\.css$/, use: ExtractTextPlugin.extract('style-loader', 'css-loader'), include: PATHS.app }
             ]
 		},
 		plugins: [
@@ -134,11 +123,9 @@ if (TARGET === 'test' || TARGET === 'tdd') {
 			}
 		},
 		module: {
-			preLoaders: [
-				{ test: /\.jsx?$/, loaders: ['isparta-instrumenter'], include: PATHS.app }
-			],
-			loaders: [
-				{ test: /\.jsx?$/, loaders: ['babel?cacheDirectory'], include: PATHS.app }
+			rules: [
+				{ test: /\.jsx?$/, use: ['isparta-instrumenter-loader'], include: PATHS.app, enforce: 'pre' },
+				{ test: /\.jsx?$/, use: ['babel-loader?cacheDirectory'], include: PATHS.app }
 			]
 		}
 	});
